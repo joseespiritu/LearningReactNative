40d6fe47220c2a6349d8e3cba3eaf92b7891159c
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-01-28 15:21:07 -0600

    LayoutsNative ejemplo

---
+++ layoutsnative/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.contenedor}>
+        <View style={styles.caja1}></View>
+        <View style={styles.caja2}></View>
+        <View style={styles.caja3}></View>
+        <View style={styles.caja4}></View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: 'cornflowerblue',
+    height: 100,
+    flex: 1
+  },
+  caja1: {
+    padding: 20,
+    backgroundColor: 'navy'
+  },
+  caja2: {
+    padding: 20,
+    backgroundColor: 'yellow'
+  },
+  caja3: {
+    padding: 20,
+    backgroundColor: 'green'
+  },
+  caja4: {
+    padding: 20,
+    backgroundColor: 'teal'
+  }
+});
+
+export default App;
