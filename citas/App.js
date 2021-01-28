73820a3106eddf53c5eb0e39fa7a1ee4873f4cb0
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-01-28 09:55:22 -0600

    Citas paciente, Screen

--- citas/App.js
+++ citas/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+import Cita from "./componentes/Cita";
 
 
 const App = () => {
   console.log("Desde consola");
+
+  // definir el state de citas
+  const [citas, setCitas] = useState([
+    { id: "1", paciente: "Hook", propietario: 'Juan', sintomas: "No come" },
+    { id: "2", paciente: "Redux", propietario: 'Iztel', sintomas: "No Duerme" },
+    { id: "3", paciente: "Native", propietario: 'Josue', sintomas: "No Canta" }
+  ]);
+
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Administrador de citas</Text>
+
+      <FlatList
+        data={citas}
+        renderItem={ ({item}) => <Cita item={item}/> }
+        keyExtractor={ cita => cita.id }
+      />
     </View>
   );
 };
