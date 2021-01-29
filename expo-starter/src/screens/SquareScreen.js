81ef4060e93adbb248bd8c39459518990691a7ba
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-01-29 10:07:31 -0600

    new validation

--- expo-starter/src/screens/SquareScreen.js
+++ expo-starter/src/screens/SquareScreen.js
@@ -10,11 +10,17 @@ const reducer = (state, action) => {
 
     switch(action.colorToChange) {
         case 'red':
-            return { ...state, red: state.red + action.amount };
+            state.red + action.amount > 255 || state.red + action.amount < 0 
+                ? state
+                : { ...state, red: state.red + action.amount };
         case 'green':
-            return { ...state, green: state.green + action.amount };
+            state.green + action.amount > 255 || state.green + action.amount < 0 
+                ? state
+                : { ...state, green: state.green + action.amount };
         case 'blue':
-            return { ...state, blue: state.blue + action.amount };
+            state.blue + action.amount > 255 || state.blue + action.amount < 0 
+                ? state
+                : { ...state, blue: state.blue + action.amount };
         default:
             return state;
     }
