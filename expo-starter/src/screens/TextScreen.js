bf821bbfbc5c65886fe46720167ee0eff053b913
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-01-29 12:27:25 -0600

    TextInput with conditional and useState

--- expo-starter/src/screens/TextScreen.js
+++ expo-starter/src/screens/TextScreen.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
+    const [password, setPassword] = useState('');
     return (
         <View>
-            <Text>Text Screen</Text>
+            <Text>Enter Password:</Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={password}
+                onChangeText={(newText) => setPassword(newText)}
+            />
+            { password.length < 5
+            ? ( <Text>Password must be longer than 5 characters</Text> )
+            : ( <Text>My Password is {password }</Text> )
+            }
+            
         </View>
     );
 };
 
 
 const styles = StyleSheet.create({
-
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+        height: 40
+    }
 });
 
 export default TextScreen;
