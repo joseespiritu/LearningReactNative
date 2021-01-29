e5c256a12b0b184ab33e15df738374b6ba3f1408
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-01-29 12:33:07 -0600

    Box system layout screens

--- expo-starter/src/screens/HomeScreen.js
+++ expo-starter/src/screens/HomeScreen.js
@@ -36,6 +36,10 @@ const HomeScreen = ({ navigation }) => {
       title="Go to Text Demo"
       onPress={() => navigation.navigate('Text')}
     />
+    <Button
+      title="Go to Box Demo"
+      onPress={() => navigation.navigate('Box')}
+    />
     {/* <TouchableOpacity>
       <Text>Go to List Demo</Text>
     </TouchableOpacity> */}
