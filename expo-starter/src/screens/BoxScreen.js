c2858dfb3ba3b9fddacafdc4bd4791d317471b68
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-02 10:41:09 -0600

    MySolution

--- expo-starter/src/screens/BoxScreen.js
+++ expo-starter/src/screens/BoxScreen.js
@@ -15,21 +15,32 @@ const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
         borderColor: 'black',
-        height: 200
+        height: 200,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     textOneStyle: {
-        borderWidth: 3,
-        borderColor: 'red'
-    },
-    textTwoStyle: {
+        backgroundColor: 'red',
+        color: '#FFF',
         borderWidth: 3,
         borderColor: 'red',
-        fontSize: 18,
-        ...StyleSheet.absoluteFillObject
+        height: 100,
+        width: 100
+    },
+    textTwoStyle: {
+        backgroundColor: 'green',
+        borderWidth: 3,
+        borderColor: 'green',
+        height: 100,
+        width: 100,
+        top: 100
     },
     textThreeStyle: {
+        backgroundColor: 'purple',
         borderWidth: 3,
-        borderColor: 'red'
+        borderColor: 'purple',
+        height: 100,
+        width: 100
     }
 });
 
