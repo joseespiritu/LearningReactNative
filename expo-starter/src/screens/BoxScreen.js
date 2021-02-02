d63cca2b18bda6defa680229e9b592466819cca3
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-02 09:29:30 -0600

    Flex AlignItems

--- expo-starter/src/screens/BoxScreen.js
+++ expo-starter/src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { Text, View, StyleSheet} from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>BoxScreen</Text>
+            <Text style={styles.textStyle}>child #1</Text>
+            <Text style={styles.textStyle}>child #2</Text>
+            <Text style={styles.textStyle}>child #3</Text>
         </View>
     );
 };
@@ -12,12 +14,12 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
-        borderColor: 'black'
+        borderColor: 'black',
+        alignItems: 'flex-end'
     },
     textStyle: {
-        borderWidth: 10,
+        borderWidth: 3,
         borderColor: 'red',
-        margin: 20
     }
 });
 
