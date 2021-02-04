1c20532be0bc0de633e58e1065873eaa3e3d979f
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-04 10:07:38 -0600

    Logic to edit Blog

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -2,6 +2,10 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch(action.type){
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id ? action.payload : blogPost;
+            });
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
@@ -29,10 +33,18 @@ const deleteBlogPost = dispatch => {
     return (id) => {
         dispatch({ type: 'delete_blogpost', payload: id });
     };
-}
+};
+
+const editBlogPost = dispatch => {
+    return (id, title, content) => {
+        dispatch({
+            type: 'edit_blogpost', 
+            payload: { id, title, content }})
+    };
+};
 
 
 export const {Context, Provider} = createDataContext(blogReducer, 
-    {addBlogPost, deleteBlogPost},
+    {addBlogPost, deleteBlogPost, editBlogPost},
     [{title:'Test Post', content: 'TEST CONTENT', id: 1}]
 );
