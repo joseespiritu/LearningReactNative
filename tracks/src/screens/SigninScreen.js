d4bcb06ad8a236ee326c228a83cb742bf3f981d6
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-05 15:52:20 -0600

    New Project Tracks, put screens and navigator

---
+++ tracks/src/screens/SigninScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+const SigningScreen = () => {
+    return (
+        <Text>SigningScreen</Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SigningScreen;
