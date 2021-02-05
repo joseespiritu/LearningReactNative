b9163971505354e76dd3baa3fbd782c6d3176e02
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-05 15:59:41 -0600

    Navigation Flow

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
     return (
+        <>
         <Text>SignupScreen</Text>
+        <Button 
+            title="Go to Signin" 
+            onPress={() => navigation.navigate('Signin')} 
+        />
+        <Button 
+            title="Go to MainFlow" 
+            onPress={() => navigation.navigate('mainFlow')} 
+        />
+        </>
     );
 };
 
