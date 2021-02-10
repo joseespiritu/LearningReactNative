228b78c46aea44b33f0f9c4ffb036a0bc27e1222
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-10 13:57:52 -0600

    Estilos a la aplicacion

--- climareact/App.js
+++ climareact/App.js
@@ -1,19 +1,36 @@
 
 import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-} from 'react-native';
+import { StyleSheet, View, Text, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Formulario from './components/Formulario';
 
 const App = () => {
+
+  const ocultarTeclado = () => {
+    Keyboard.dismiss();
+  }
+
   return (
-    <Formulario />
+    <>
+    <TouchableWithoutFeedback onPress={() => ocultarTeclado()}>
+      <View style={styles.app}>
+        <View style={styles.contenido}>
+        <Formulario />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+    </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: 'rgb(71,149,212)',
+    justifyContent: 'center'
+  },
+  contenido: {
+    marginHorizontal: '2.5%'
+  }
+});
 
 export default App;
--- climareact/components/Formulario.js
+++ climareact/components/Formulario.js
@@ -1,25 +1,69 @@
 import React from 'react';
-import { Text, TextInput, View, StyleSheet } from "react-native";
+import { Text, TextInput, View, StyleSheet, TouchableWithoutFeedback } from "react-native";
+import { Picker } from '@react-native-community/picker';
 
 const Formulario = () => {
     return (
         <>
-            <View
-                style={styles.formulario}>
+            <View>
                 <View>
                     <TextInput
+                    style={styles.input}
                         placeholder="Ciudad"
                         placeholderTextColor='#000'
                     />
                 </View>
+                <View>
+                    <Picker
+                        style={styles.pickerStyle}
+                    >
+                        <Picker.Item label="-- Seleccione un país --" value=""/>
+                        <Picker.Item label="Estados Unidos" value="US"/>
+                        <Picker.Item label="México" value="MX"/>
+                        <Picker.Item label="Argentina" value="AR"/>
+                        <Picker.Item label="Colombia" value="CO"/>
+                        <Picker.Item label="Costa Rica" value="CR"/>
+                        <Picker.Item label="España" value="ES"/>
+                        <Picker.Item label="Perú" value="PE"/>
+                    </Picker>
+                </View>
+                <TouchableWithoutFeedback>
+                    <View 
+                        style={styles.btnBuscar}
+                    >
+                        <Text style={styles.textoBuscar}>Buscar Clima</Text>
+                    </View>
+                </TouchableWithoutFeedback>
             </View>
         </>
     );
 };
 
 const styles = StyleSheet.create({
-    formulario: {
-        marginTop: 100
+    input: {
+        padding: 10,
+        height: 50,
+        backgroundColor: '#FFF',
+        fontSize: 20,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    pickerStyle: {
+        height: 80,
+        backgroundColor: '#FFF'
+    },
+    btnBuscar: {
+        marginTop: 50,
+        backgroundColor: '#000',
+        padding: 10,
+        justifyContent: 'center'
+    },
+    textoBuscar: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        fontSize: 18
     }
 })
 
