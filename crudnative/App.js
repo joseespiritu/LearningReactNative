815892beea3f51a622aee55b8ef3858108b42cf0
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-11 14:33:02 -0600

    React native init project CRUD

---
+++ crudnative/App.js
@@ -0,0 +1,56 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import Inicio from './views/Inicio';
+import NuevoCliente from './views/NuevoCliente';
+import DetallesCliente from './views/DetallesCliente';
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Inicio"
+        >
+          <Stack.Screen
+            name="Inicio"
+            component={Inicio}
+          />
+          <Stack.Screen
+            name="NuevoCliente"
+            component={NuevoCliente}
+            options={{
+              title: "Nuevo Cliente"
+            }}
+          />
+          <Stack.Screen
+            name="DetallesCliente"
+            component={DetallesCliente}
+            options={{
+              title: "Detalles Cliente"
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
---
+++ crudnative/views/NuevoCliente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from "react-native";
+
+const NuevoCliente = () => {
+    return (
+        <Text>
+            Desde NuevoCliente
+        </Text>
+    );
+}
+
+export default NuevoCliente;
