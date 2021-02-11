d682c65a42deacc20c9e2b5445d453323e649779
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-11 09:31:48 -0600

    Spring

--- animacionesnative/App.js
+++ animacionesnative/App.js
@@ -3,14 +3,15 @@ import {StyleSheet,ScrollView,View,Text} from 'react-native';
 //import Animacion1 from './components/Animacion1';
 //import Animacion2 from './components/Animacion2';
 //import Animacion3 from './components/Animacion3';
-import Animacion4 from './components/Animacion4';
+//import Animacion4 from './components/Animacion4';
+import Animacion5 from './components/Animacion5';
 
 
 const App = () => {
   return (
     <>
       <View style={styles.contenido}>
-        <Animacion4/>
+        <Animacion5/>
       </View>
     </>
   );
