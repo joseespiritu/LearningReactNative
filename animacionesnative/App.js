88a254d01e2a3d423c5fe9daa7d49c59b36ccf8e
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-11 09:46:09 -0600

    Sequence

--- animacionesnative/App.js
+++ animacionesnative/App.js
@@ -4,14 +4,15 @@ import {StyleSheet,ScrollView,View,Text} from 'react-native';
 //import Animacion2 from './components/Animacion2';
 //import Animacion3 from './components/Animacion3';
 //import Animacion4 from './components/Animacion4';
-import Animacion5 from './components/Animacion5';
+//import Animacion5 from './components/Animacion5';
+import Animacion6 from './components/Animacion6';
 
 
 const App = () => {
   return (
     <>
       <View style={styles.contenido}>
-        <Animacion5/>
+        <Animacion6/>
       </View>
     </>
   );
