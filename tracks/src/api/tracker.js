120130a2cb8c5bddf221d0dc24b786a219e6e11f
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-12 13:37:32 -0600

    Connection with API signup, show message error

---
+++ tracks/src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://5fa333fc18f6.ngrok.io'
+});
--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,15 +1,24 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
     switch(action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload };
         default:
             return state;
     }
 };
 
 const signup = (dispatch) => {
-    return ({ email, password }) => {
+    return async ({ email, password }) => {
         // make api requests to sign up with that email and password
+        try {
+            const response = await trackerApi.post('/signup', {email,password});
+            console.log(response.data);
+        } catch (err) {
+            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' });
+        }
 
         // if we sign up, modify our state, and say that we are authenticated
 
@@ -35,5 +44,5 @@ const signout = (dispatch) => {
 export const {Provider, Context} = createDataContext(
     authReducer,
     { signin, signout, signup },
-    { isSignedIn: false }
+    { isSignedIn: false, errorMessage: '' }
 );
