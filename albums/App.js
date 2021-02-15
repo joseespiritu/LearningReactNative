c8f933f60f09ec82e3e755d6cb84dd9fce46fef3
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-15 11:56:22 -0600

    Albums App Init and first component

---
+++ albums/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from "react-native";
+import Header from './src/components/header';
+
+const App = () => {
+  return (
+      <Header headerText={'Albums'}/>
+  );
+};
+
+export default App;
