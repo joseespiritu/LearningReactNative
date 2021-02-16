abff206cf6d159bf7e0540601fbf6c6fdf9f4770
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-16 16:42:21 -0600

    Configuracion y conexion a Firebase

--- RestauranteApp/restaurantcliente/README.md
+++ RestauranteApp/restaurantcliente/README.md
@@ -3,3 +3,4 @@
 ## Dependencias
 * Instalación de formik: _npm i formik_
 * Validación de formularios con yup: _npm i yup_
+* Instalación de Firebase: _npm i firebase_
---
+++ RestauranteApp/restaurantcliente/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCavAGZ5umVUg4APYXHxT405GT-8zXXjFA",
+    authDomain: "restaurant-61397.firebaseapp.com",
+    projectId: "restaurant-61397",
+    storageBucket: "restaurant-61397.appspot.com",
+    messagingSenderId: "359180826173",
+    appId: "1:359180826173:web:16f116f11623ab37059c54",
+    measurementId: "G-REF23Q18ZC"
+};
+
+export default firebaseConfig;
