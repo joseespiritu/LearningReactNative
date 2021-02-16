bae9065f692668bb43606ecc7ec618f5819f8413
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-16 13:12:59 -0600

    Formulario para platillos

--- RestauranteApp/restaurantcliente/src/components/paginas/NuevoPlatillo.js
+++ RestauranteApp/restaurantcliente/src/components/paginas/NuevoPlatillo.js
@@ -3,7 +3,72 @@ import React from 'react';
 const NuevoPlatillo = () => {
     return ( 
         <>
-            <h1 className="text-3xl font-light mb-4">NuevoPlatillo</h1>
+            <h1 className="text-3xl font-light mb-4">Agregar Platillo</h1>
+
+            <div className="flex justify-center mt-10">
+                <div className="w-full max-w-3xl">
+                    <form>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">Nombre</label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="nombre"
+                                type="text"
+                                placeholder="Nombre Platillo"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="precio">Precio</label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="precio"
+                                type="number"
+                                placeholder="$20"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="categoria">Categoria</label>
+                            <select
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="precio"
+                                name="categoria"
+                            >
+                                <option value="">-- Seleccione --</option>
+                                <option value="desayuno">Desayuno</option>
+                                <option value="comida">Comida</option>
+                                <option value="cena">Cena</option>
+                                <option value="bebidas">Bebidas</option>
+                                <option value="postre">Postre</option>
+                                <option value="ensaladas">Ensaladas</option>
+                            </select>
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen">Imagen</label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="imagen"
+                                type="file"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="descripcion">Descripción</label>
+                            <textarea
+                                className="h-40 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="descripcion"
+                                type="text"
+                                placeholder="Descripción del platillo"
+                            >
+
+                            </textarea>
+                        </div>
+                        <input
+                            type="submit"
+                            className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold"
+                            value="Agregar Platillo"
+                        />
+                    </form>
+                </div>
+            </div>
         </>
      );
 }
