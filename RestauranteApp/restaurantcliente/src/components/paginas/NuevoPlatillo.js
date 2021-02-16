ba8c113ef5d3f97cbc56b66850dcfba6156c4ee3
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-16 16:50:16 -0600

    Instanciando Firestore

--- RestauranteApp/restaurantcliente/src/components/paginas/NuevoPlatillo.js
+++ RestauranteApp/restaurantcliente/src/components/paginas/NuevoPlatillo.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { FirebaseContext } from "../../firebase";
 
 const NuevoPlatillo = () => {
+
+    // Context con las operaciones de Firebase
+    const { firebase } = useContext(FirebaseContext);
+    console.log(firebase )
 
     // Validacion y leer los datos del formulario
     const formik = useFormik({
