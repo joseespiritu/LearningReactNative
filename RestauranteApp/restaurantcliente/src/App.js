0938dcb8c2c7578dec185d40c11752fa4dab6cf0
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-16 11:53:23 -0600

    Nuevas secciones, estilo y NavLink

--- RestauranteApp/restaurantcliente/src/App.js
+++ RestauranteApp/restaurantcliente/src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Routes, Route } from "react-router";
 
 import Ordenes from "./components/paginas/Ordenes";
+import Menu from "./components/paginas/Menu";
+import NuevoPlatillo from "./components/paginas/NuevoPlatillo";
+import Sidebar from "./components/ui/Sidebar";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Ordenes/>} />
-      </Routes>
+    <div className="md:flex  min-h-screen">
+      <Sidebar/>
+      <div className="md:w-3/5 xl:w-4/5">
+        <Routes>
+          <Route path="/" element={<Ordenes/>} />
+          <Route path="/menu" element={<Menu/>} />
+          <Route path="/nuevo-platillo" element={<NuevoPlatillo/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ RestauranteApp/restaurantcliente/src/components/paginas/NuevoPlatillo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NuevoPlatillo = () => {
+    return ( 
+        <>
+            <h1 className="text-3xl font-light mb-4">NuevoPlatillo</h1>
+        </>
+     );
+}
+ 
+export default NuevoPlatillo;
