91b6d3806dd16babee44b0cc9a90766482725e61
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-16 16:28:00 -0600

    Validaciones con Yup dentro de formulario

--- RestauranteApp/restaurantcliente/README.md
+++ RestauranteApp/restaurantcliente/README.md
@@ -2,3 +2,4 @@
 
 ## Dependencias
 * Instalación de formik: _npm i formik_
+* Validación de formularios con yup: _npm i yup_
--- RestauranteApp/restaurantcliente/src/components/paginas/NuevoPlatillo.js
+++ RestauranteApp/restaurantcliente/src/components/paginas/NuevoPlatillo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const NuevoPlatillo = () => {
 
@@ -12,6 +13,19 @@ const NuevoPlatillo = () => {
             imagen: '',
             descripcion: '',
         },
+        validationSchema: Yup.object({
+            nombre: Yup.string()
+                        .min(3, 'Los platillos deben tener al menos 3 caracteres')
+                        .required('El Nombre del platillo es obligatorio'),
+            precio: Yup.number()
+                        .min(1, 'Debes agregar un número')
+                        .required('El Precio es obligatorio'),
+            categoria: Yup.string()
+                        .required('La categoria es obligatoria'),
+            descripcion: Yup.string()
+                        .min(10, 'La descripcion debe de ser más larga')
+                        .required('La descripción es obligatoria'),
+        }),
         onSubmit: datos => {
             console.log(datos)
         }
@@ -36,8 +50,17 @@ const NuevoPlatillo = () => {
                                 placeholder="Nombre Platillo"
                                 value={formik.values.nombre}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
                         </div>
+
+                        { formik.touched.nombre && formik.errors.nombre ? (
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4" role="alert">
+                                <p className="font-bold">Hubo un error</p>
+                                <p>{formik.errors.nombre}</p>
+                            </div>
+                        ) : null }
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="precio">Precio</label>
                             <input
@@ -47,8 +70,17 @@ const NuevoPlatillo = () => {
                                 placeholder="$20"ç
                                 value={formik.values.precio}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
                         </div>
+
+                        { formik.touched.precio && formik.errors.precio ? (
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4" role="alert">
+                                <p className="font-bold">Hubo un error</p>
+                                <p>{formik.errors.precio}</p>
+                            </div>
+                        ) : null }
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="categoria">Categoria</label>
                             <select
@@ -57,6 +89,7 @@ const NuevoPlatillo = () => {
                                 name="categoria"
                                 value={formik.values.categoria}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             >
                                 <option value="">-- Seleccione --</option>
                                 <option value="desayuno">Desayuno</option>
@@ -67,6 +100,14 @@ const NuevoPlatillo = () => {
                                 <option value="ensaladas">Ensaladas</option>
                             </select>
                         </div>
+
+                        { formik.touched.categoria && formik.errors.categoria ? (
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4" role="alert">
+                                <p className="font-bold">Hubo un error</p>
+                                <p>{formik.errors.categoria}</p>
+                            </div>
+                        ) : null }
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen">Imagen</label>
                             <input
@@ -75,6 +116,7 @@ const NuevoPlatillo = () => {
                                 type="file"
                                 value={formik.values.imagen}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
                         </div>
                         <div className="mb-4">
@@ -86,10 +128,19 @@ const NuevoPlatillo = () => {
                                 placeholder="Descripción del platillo"
                                 value={formik.values.descripcion}
                                 onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             >
 
                             </textarea>
                         </div>
+                        
+                        { formik.touched.descripcion && formik.errors.descripcion ? (
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-4" role="alert">
+                                <p className="font-bold">Hubo un error</p>
+                                <p>{formik.errors.descripcion}</p>
+                            </div>
+                        ) : null }
+
                         <input
                             type="submit"
                             className="bg-gray-800 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold"
