86c7b191536b58dc481fa3942843643b054dfc6e
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-17 09:51:13 -0600

    Obteniendo platillos en tiempo real

--- RestauranteApp/restaurantcliente/src/components/paginas/Menu.js
+++ RestauranteApp/restaurantcliente/src/components/paginas/Menu.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from "react-router-dom";
+import { FirebaseContext } from "../../firebase";
 
 const Menu = () => {
+
+    // Definir el state para los platillos
+    const [platillos, guardarPlatillos ] = useState([]);
+
+    const { firebase } = useContext(FirebaseContext);
+
+    // consultar la base de datos al cargar
+    useEffect(() => {
+        const obtenerPlatillos = () => {
+            // Activando la funcion en tiempo real
+            firebase.db.collection("productos").onSnapshot(manejarSnapshot);
+        }
+        obtenerPlatillos();
+    }, []);
+
+    // Snapshot nos permite utilizar la base de datos en tiempo real
+    function manejarSnapshot(snapshot){
+        const platillos = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+
+        console.log(platillos);
+        // Almacenar platillos en useState
+        guardarPlatillos(platillos);
+    }
+
     return ( 
         <>
             <h1 className="text-3xl font-light mb-4">Menu</h1>
