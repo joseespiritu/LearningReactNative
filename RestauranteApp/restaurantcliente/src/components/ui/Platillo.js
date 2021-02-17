84ae19f72ff3116a45ebb45dbb023eba3926ba5f
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-17 13:16:43 -0600

    Agregando select, cambiando existencia de producto

--- RestauranteApp/restaurantcliente/src/components/ui/Platillo.js
+++ RestauranteApp/restaurantcliente/src/components/ui/Platillo.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useContext, useRef } from 'react';
+import { FirebaseContext } from "../../firebase";
 
 const Platillo = ({platillo}) => {
 
-    const { nombre, imagen, existencia, categoria, precio, descripcion } = platillo;
+    // Existencia ref para acceder al valor directamente
+    const existenciaRef = useRef(platillo.existencia);
+
+    // context de firebase para cambios en la DB
+    const { firebase } = useContext(FirebaseContext);
+
+    const { id, nombre, imagen, existencia, categoria, precio, descripcion } = platillo;
+
+    // Modificar estado de platillo en firebase
+    const actualizarDisponibilidad = () => {
+
+        const existencia = (existenciaRef.current.value == "true");
+
+        try {
+            firebase.db.collection('productos')
+            .doc(id)
+            .update({
+                existencia
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return ( 
         <div className="w-full px-3 mb-4">
@@ -10,6 +33,20 @@ const Platillo = ({platillo}) => {
                 <div className="lg:flex">
                     <div className="lg:w-5/12 xl:w-3/12">
                         <img src={imagen} alt=" imagen platillo" />
+                        <div className="sm:flex sm:-mx-2">
+                            <label className="block mt-5 sm:w-2/4 pl-2">
+                                <span className="block text-gray-800 mb-2">Existencia</span>
+                                <select 
+                                className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-white "
+                                value={existencia}
+                                ref={existenciaRef}
+                                onChange={() => actualizarDisponibilidad()}
+                                >
+                                    <option value="true">Disponible</option>
+                                    <option value="false">No Disponible</option>
+                                </select>
+                            </label>
+                        </div>
                     </div>
                     <div className="lg:w-7/12 xl:w-9/12 pl-5">
                         <p className="font-bold text-2xl text-yellow-600 mb-4">
