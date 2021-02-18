63dd7c849652b3a7d1257299818ad4a3df4d35b5
JoseSPS <jlmartinez@spsolutions.com.mx>
2021-02-18 09:25:08 -0600

    Restaurante App Móvil, definiendo navegacion

---
+++ RestauranteApp/restaurantapp/views/FormularioPlatillo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from "react-native";
+
+const FormularioPedido = () => {
+    return ( 
+        <Text>FormularioPedido</Text>
+     );
+}
+ 
+export default FormularioPedido;
