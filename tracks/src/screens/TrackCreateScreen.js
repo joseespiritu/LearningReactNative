e5607d05bddf41a994a506be63ceaa3cdd8ee517
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-03-13 21:00:55 -0600

    check current Screen Map

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -2,16 +2,18 @@
 import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from "react-native-elements";
-import { SafeAreaView } from 'react-navigation';
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
 
   const { addLocation } = useContext(LocationContext);
 
   const [err] = useLocation(addLocation);
+
+  console.log(isFocused);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
@@ -24,4 +26,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
